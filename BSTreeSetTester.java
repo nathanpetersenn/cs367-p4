1dea730d0c14880f08f2a179b51061581d98b50d
edegler <evandegler1@gmail.com>
2016-04-08 21:29:07 -0500

    Added files via upload

---
+++ BSTreeSetTester.java
@@ -0,0 +1,211 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * SetTesterADT implementation using a Binary Search Tree (BST) as the data
+ * structure.
+ *
+ * <p>The BST rebalances if a specified threshold is exceeded (explained below).
+ * If rebalanceThreshold is non-positive (&lt;=0) then the BST DOES NOT
+ * rebalance. It is a basic BStree. If the rebalanceThreshold is positive
+ * then the BST does rebalance. It is a BSTreeB where the last B means the
+ * tree is balanced.</p>
+ *
+ * <p>Rebalancing is triggered if the absolute value of the balancedFfactor in
+ * any BSTNode is &gt;= to the rebalanceThreshold in its BSTreeSetTester.
+ * Rebalancing requires the BST to be completely rebuilt.</p>
+ *
+ * @author CS367
+ */
+public class BSTreeSetTester <K extends Comparable<K>> implements SetTesterADT<K>{
+
+    /** Root of this tree */
+    BSTNode<K> root;
+
+    /** Number of items in this tree*/
+    int numKeys;
+
+    /**
+     * rebalanceThreshold &lt;= 0 DOES NOT REBALANCE (BSTree).<br>
+     * rebalanceThreshold  &gt;0 rebalances the tree (BSTreeB).
+      */
+    int rebalanceThreshold;
+
+    /**
+     * True iff tree is balanced, i.e., if rebalanceThreshold is NOT exceeded
+     * by absolute value of balanceFactor in any of the tree's BSTnodes.Note if
+     * rebalanceThreshold is non-positive, isBalanced must be true.
+     */
+    boolean isBalanced;
+
+
+    /**
+     * Constructs an empty BSTreeSetTester with a given rebalanceThreshold.
+     *
+     * @param rbt the rebalance threshold
+     */
+    public BSTreeSetTester(int rbt) {
+        //TODO
+    }
+
+    /**
+     * Add node to binary search tree. Remember to update the height and
+     * balancedFfactor of each node. Also rebalance as needed based on
+     * rebalanceThreshold.
+     *
+     * @param key the key to add into the BST
+     * @throws IllegalArgumentException if the key is null
+     * @throws DuplicateKeyException if the key is a duplicate
+     */
+    public void add(K key) {
+        //TODO
+    }
+
+    /**
+     * Rebalances the tree by:
+     * 1. Copying all keys in the BST in sorted order into an array.
+     *    Hint: Use your BSTIterator.
+     * 2. Rebuilding the tree from the sorted array of keys.
+     */
+    public void rebalance() {
+        K[] keys = (K[]) new Comparable[numKeys];
+        //TODO
+    }
+
+    /**
+     * Recursively rebuilds a binary search tree from a sorted array.
+     * Reconstruct the tree in a way similar to how binary search would explore
+     * elements in the sorted array. The middle value in the sorted array will
+     * become the root, the middles of the two remaining halves become the left
+     * and right children of the root, and so on. This can be done recursively.
+     * Remember to update the height and balanceFactor of each node.
+     *
+     * @param keys the sorted array of keys
+     * @param start the first index of the part of the array used
+     * @param stop the last index of the part of the array used
+     * @return root of the new balanced binary search tree
+     */
+    private BSTNode<K> sortedArrayToBST(K[] keys, int start, int stop) {
+        //TODO
+    	if  (stop - start == 0) return null;
+    	
+    	int mid = (stop - start) / 2;
+    	
+    	BSTNode<K> ret = new BSTNode<K>(keys[mid]);
+    	ret.setLeftChild(sortedArrayToBST(keys, start, mid));
+    	ret.setRightChild(sortedArrayToBST(keys, mid, stop));
+    	
+    	ret.setHeight(h);
+    	ret.setBalanceFactor(bf);
+    
+    	return ret;
+    }
+
+    /**
+     * Returns true iff the key is in the binary search tree.
+     *
+     * @param key the key to search
+     * @return true if the key is in the binary search tree
+     * @throws IllegalArgumentException if key is null
+     */
+    public boolean contains(K key) {
+        //TODO
+    	if (key == null) throw new IllegalArgumentException();
+        return contains(root, key);
+    }
+    
+    // Private companion contains method
+    private boolean contains(BSTNode<K> root, K key){
+    	if (root == null) return false;
+    	
+    	// If the root's key is greater than key, look in left subtree
+    	if (root.getKey().compareTo(key) > 0) {
+    		contains(root.getLeftChild(), key);
+    	}
+    	// If the root's key is less than key, look in right subtree
+    	else if (root.getKey().compareTo(key) < 0) {
+    		contains(root.getRightChild(), key);
+    	}
+
+    	return true;
+    }
+    
+    /**
+     * Returns the sorted list of keys in the tree that are in the specified
+     * range (inclusive of minValue, exclusive of maxValue). This can be done
+     * recursively.
+     *
+     * @param minValue the minimum value of the desired range (inclusive)
+     * @param maxValue the maximum value of the desired range (exclusive)
+     * @return the sorted list of keys in the specified range
+     * @throws IllegalArgumentException if either minValue or maxValue is
+     * null, or minValue is larger than maxValue
+     */
+    public List<K> subSet(K minValue, K maxValue) {
+        //TODO - Not sure if minVal.compareTo(maxVal) GREATER THAN 0 is right
+    	if (minValue == null || maxValue == null || minValue.compareTo(maxValue) > 0){
+    		throw new IllegalArgumentException();
+    	}
+    	
+    	
+        return null;
+    }
+
+    /**
+     * Return an iterator for the binary search tree.
+     * @return the iterator
+     */
+    public Iterator<K> iterator() {
+        //TODO
+        return new BSTIterator<K>(root);
+    }
+
+    /**
+     * Clears the tree, i.e., removes all the keys in the tree.
+     */
+    public void clear() {
+        //TODO
+    	numKeys = 0;
+    	root = null;
+    }
+
+    /**
+     * Returns the number of keys in the tree.
+     *
+     * @return the number of keys
+     */
+    public int size() {
+        return numKeys;
+    }
+
+    /**
+     * Displays the top maxNumLevels of the tree. DO NOT CHANGE IT!
+     *
+     * @param maxDisplayLevels from the top of the BST that will be displayed
+     */
+    public void displayTree(int maxDisplayLevels) {
+        if (rebalanceThreshold > 0) {
+            System.out.println("---------------------------" +
+                    "BSTreeBSet Display-------------------------------");
+        } else {
+            System.out.println("---------------------------" +
+                    "BSTreeSet Display--------------------------------");   
+        }
+        displayTreeHelper(root, 0, maxDisplayLevels);
+    }
+
+    private void displayTreeHelper(BSTNode<K> n, int curDepth,
+                                   int maxDisplayLevels) {
+        if(maxDisplayLevels <= curDepth) return;
+        if (n == null)
+            return;
+        for (int i = 0; i < curDepth; i++) {
+            System.out.print("|--");
+        }
+        System.out.println(n.getKey() + "[" + n.getHeight() + "]{" +
+                n.getBalanceFactor() + "}");
+        displayTreeHelper(n.getLeftChild(), curDepth + 1, maxDisplayLevels);
+        displayTreeHelper(n.getRightChild(), curDepth + 1, maxDisplayLevels);
+    }
+}
