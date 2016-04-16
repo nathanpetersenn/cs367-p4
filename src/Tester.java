d87c27f8f3646eaf832e2a480499ef1a229789d5
edegler <evandegler1@gmail.com>
2016-04-16 13:47:53 -0500

    Tester changes. See FB messenger for explanations for why stuff doesn't work

--- src/Tester.java
+++ src/Tester.java
@@ -3,14 +3,19 @@ public class Tester {
 	public static void main(String[] args){
 		
 		BSTreeSetTester<Integer> bst = new BSTreeSetTester<Integer>(-1);
-		for (int i=1; i<25; i++){
+		for (int i=1; i<8; i++){
 			bst.add(i);
+			System.out.println("---------------------------BEFORE REBALANCE");
+			bst.displayTree(50);
+			bst.rebalance();
+			System.out.println("---------------------------AFTER REBALANCE");
+			bst.displayTree(50);
 		}
 		
-		bst.displayTree(50);
+		//bst.displayTree(50);
 		
-		bst.rebalance();
-		bst.displayTree(50);
+//		bst.rebalance();
+//		bst.displayTree(50);
 
 	}	
 }
